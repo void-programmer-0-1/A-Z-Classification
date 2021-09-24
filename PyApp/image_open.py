f4028512e0226e885e5f1baf485fd1a62a5df498
santoshm <santoshm@digikriya.com>
2021-09-24 13:27:01 +0530

    created v6

--- PyApp/image_open.py
+++ PyApp/image_open.py
@@ -1,11 +1,9 @@
-import cv2
 
-image = cv2.imread("image.png")
 
-image = image * 255
+import matplotlib.pyplot as plt
 
+image = plt.imread("image.png")
 print(image.shape)
 
-cv2.imshow("image",image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+plt.imshow(image)
+plt.show()
